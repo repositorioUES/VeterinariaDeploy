3edb07a3193b13217ddabfc16c167a5da9b6316e
Christian Garcia <kriz10@outlook.com>
2022-06-21 17:04:57 -0600

    db

--- ProyectoVeterinaria/settings.py
+++ ProyectoVeterinaria/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.contrib import staticfiles
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -127,10 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = BASE_DIR, 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 STATICFILES_DIRS=[
