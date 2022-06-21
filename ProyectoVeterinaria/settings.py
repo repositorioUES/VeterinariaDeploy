70296db11cf8aa70a2735b2f1158dcb1157888c3
Christian Garcia <kriz10@outlook.com>
2022-06-21 17:13:10 -0600

    db

--- ProyectoVeterinaria/settings.py
+++ ProyectoVeterinaria/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = BASE_DIR, 'static'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS=[
