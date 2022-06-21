a82c82dc89ead5c7138aa4196210741cdaebe300
Christian Garcia <kriz10@outlook.com>
2022-06-21 17:47:31 -0600

    .

--- ProyectoVeterinaria/settings.py
+++ ProyectoVeterinaria/settings.py
@@ -129,12 +129,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-]
-
-STATICFILES_DIRS = BASE_DIR, 'static'
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root')
+STATICFILES_DIRS = BASE_DIR, 'static'
+
 
 
 STATICFILES_DIRS=[
